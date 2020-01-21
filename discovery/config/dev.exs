14ce5a4a3046b24d59c4ea9d23a36596f8f4bef1
esearcy <esearcy@mediciventures.com>
2020-01-21 12:52:29 -0700

    removed unnecessary files

--- discovery/config/dev.exs
+++ discovery/config/dev.exs
@@ -4,7 +4,7 @@ use Mix.Config
 config :discovery, Discovery.Repo,
   username: System.get_env("POSTGRES_USER") || "postgres",
   password: System.get_env("POSTGRES_PASSWORD") || "postgres",
-  database: System.get_env("POSTGRES_DB") || "gondor_dev",
+  database: System.get_env("POSTGRES_DB") || "discovery_dev",
   hostname: System.get_env("POSTGRES_HOST") || "localhost",
   port: System.get_env("POSTGRES_PORT") || "5432",
   show_sensitive_data_on_connection_error: true,
--- discovery/config/prod.exs
+++ discovery/config/prod.exs
@@ -1,14 +1,1 @@
 use Mix.Config
-
-config :discovery, DiscoveryWeb.Endpoint,
-  load_from_system_env: true,
-  url: [
-    host: Application.get_env(:discovery, :app_hostname),
-    port: Application.get_env(:discovery, :app_port)
-  ],
-  cache_static_manifest: "priv/static/cache_manifest.json"
-
-# Do not print debug messages in production
-config :logger, level: :info
-
-config :discovery, DiscoveryWeb.Endpoint, server: true
